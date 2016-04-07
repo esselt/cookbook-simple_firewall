5fe7fe1c15b9895377d15f56f1901f4d7cad085a
Boye Holden <boye.holden@hist.no>
2016-04-07 11:42:33 +0200

    Updated IPv6 rules to closely match IPv4 rules

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ CHANGELOG
 
 This file is used to list changes made in each version of the simple_firewall cookbook.
 
+0.1.1
+-----
+- [Boye Holden] - Updated IPv6 rules to closely match IPv4 rules
+
 0.1.0
 -----
 - [Boye Holden] - Initial release of simple_firewall
--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'simple_firewall'
 maintainer       'Boye Holden'
-maintainer_email 'boye.holden@hist.no'
+maintainer_email 'boye.holden@ntnu.no'
 license          'Apache 2.0'
 description      'A super simple IPTABLES firewall cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 recipe 'simple_firewall', 'Enables a basic firewall used with templates'
 recipe 'simple_firewall::disable', 'Disables the basic firewall'
